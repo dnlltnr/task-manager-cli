397efffcd91a627de38f469d7982582b57c3d0e8
Daniel Leitner <DaniLeit@seznam.cz>
2026-03-19 10:32:28 +0100

    make class Task JSON ready (to_dict, from_dict)

--- inventory/models.py
+++ inventory/models.py
@@ -1,13 +1,24 @@
 
 class Task:
-    def __init__(self, id, title):
+    def __init__(self, id, title, completed=False):
         self.id = id
         self.title = title
-        self.completed = False
+        self.completed = completed
 
     def __repr__(self):
         return f"id={self.id}, title={self.title}, completed={self.completed}"
     
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "completed": self.completed
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(data["id"], data["title"], data["completed"])
+
     def complete(self):
         self.completed = True
 
